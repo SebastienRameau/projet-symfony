fd9898485b527f587edaa1bcf640cd65b2545a81
Sebastien RAMEAU <sebastien.rameau2021@campus-eni.fr>
2022-03-09 16:45:36 +0100

    Accueil _ Sébastien _ 16h45

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Etat;
+use App\Repository\CampusRepository;
+use App\Repository\EtatRepository;
+use App\Repository\ParticipantRepository;
+use App\Repository\SortieRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    /**
+     * @Route("/", name="sorties")
+     */
+    public function sorties(CampusRepository $repoCampus, SortieRepository $repoSortie, EtatRepository $repoEtat,
+         ParticipantRepository $repoParticipant): Response
+    {
+        //Envoyer la date du jour
+        $date = explode("/",date('d/m/Y'));
+        list($day,$month,$year) = $date;
+        $dateJour = $day.'/'.$month.'/'.$year;
+
+        
+        //Envoyer le participant connecté (Voir plus tard, quand Estelle aura fait la connexion)
+        $participantConnecte = $repoParticipant->findOneBy(['id' => '1030']); //temporaire
+
+
+        //Envoyer la liste des campus
+        $campusListe = $repoCampus->findAll();
+
+
+        //Envoyer la liste des sorties Sauf historisées Ou non publiées (créées) (sauf si le participant est l'organisateur)
+        $requete = $repoSortie->createQueryBuilder('p')
+            ->select('p')
+            ->where('p.etat = '.$repoEtat->findOneBy(['libelle' => 'Ouverte'])->getId())
+            ->orWhere('p.etat = '.$repoEtat->findOneBy(['libelle' => 'Cloturée'])->getId())
+            ->orWhere('p.etat = '.$repoEtat->findOneBy(['libelle' => 'Activité en cours'])->getId())
+            ->orWhere('p.etat = '.$repoEtat->findOneBy(['libelle' => 'Passée'])->getId())
+            ->orWhere('p.etat = '.$repoEtat->findOneBy(['libelle' => 'Annulée'])->getId())
+            ->orWhere('p.organisateur = '.$participantConnecte->getId())
+            ->orderBy('p.dateHeureDebut', 'ASC')
+            ->getQuery();
+
+        $sortiesListe = $requete->getResult();
+
+
+        //Gérer le bouton Rechercher
+        //Ou tout faire en Javascript ?
+
+
+        //Gérer le bouton Créer une sortie
+        //Non : rediriger vers la méthode de création au click sur le bouton
+
+
+        return $this->render('sortie/index.html.twig', [
+            'date_jour' => $dateJour,
+            'participant_connecte' => $participantConnecte,
+            'campus_liste' => $campusListe,
+            'sorties_liste' => $sortiesListe,
+        ]);
+    }
+}
